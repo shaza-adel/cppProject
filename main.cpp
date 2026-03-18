7608e1345ed234e2a43627d14a1ec4560e1e4a69
Shaza adel <20251700388@cis.asu.edu.eg>
2026-03-18 11:28:34 +0200

    creating user struct

--- main.cpp
+++ main.cpp
@@ -1,2 +1,20 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
+struct UserAccount
+{
+	int UserID;
+	string Username;
+	string Password;
+};
+UserAccount Users[10];
+void main()
+{
+	logmenu();
+}
+int logmenu()
+{
+	cout << "/n╔═════════════════════════════════════════════════════╗";
+	cout << "║               ❤️Digital Mood Tracker❤️               ║/n";
+
+	
+}
